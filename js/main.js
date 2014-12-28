0ba30a049f1db46c3b227177a26fcc4269ccda39
Nirmal <njbenann@uwaterloo.ca>
2014-12-27 23:56:32 -0500

    Adding changes for mobile

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function () {
-    $(window).stellar();
+    
+    var ua = navigator.userAgent,
+    isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
+
+    if (isMobileWebkit) {
+        $('html').addClass('mobile');
+    }
+
+    $(function() {
+        var iScrollInstance;
+
+        if (isMobileWebkit) {
+            iScrollInstance = new iScroll('wrapper');
+
+            $('#scroller').stellar({
+                scrollProperty: 'transform',
+                positionProperty: 'transform',
+                horizontalScrolling: false,
+                verticalOffset: 150
+            });
+        } else {
+            $.stellar({
+                horizontalScrolling: false,
+                verticalOffset: 150
+            });
+        }
+    });
 
     var links = $('.navigation').find('li');
     slide = $('.slide');
     mywindow = $(window);
     htmlbody = $('html,body');
 
-
-    //Setup waypoints plugin
     slide.waypoint(function (event, direction) {
 
         dataslide = $(this).attr('data-slide');
