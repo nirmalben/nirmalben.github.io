6c0d2697da5e4e959d206cd010c0daa6febc4f77
Nirmal <njbenann@uwaterloo.ca>
2015-02-14 23:49:58 -0500

    Added 'From the Holy Bible' section Added 'Listening To' section. This is disabled for now.

--- js/main.js
+++ js/main.js
@@ -15,5 +15,42 @@ $(document).ready(function () {
         var hamburgerButton = $('.navbar-header > button');
         if (hamburgerButton.is(':visible'))
         	hamburgerButton.trigger('click');	// Close the dropdown
+        scroll(0,0);
     });
+
+    $.getScript('https://platform.twitter.com/widgets.js', function() {
+        //calling method load
+        twttr.widgets.load();
+        
+        // Get the latest tweet after the widget is loaded
+        twttr.events.bind(
+            'loaded',
+            function (event) {
+                getlatestTweetText();
+            }); 
+    });
+
+    // Function to get the latest tweet using javascript
+    function getlatestTweetText() {
+        var twitterWidgetIframe = document.getElementById("twitter-widget-0").contentDocument;
+        var latestTweet = (twitterWidgetIframe.getElementsByClassName('stream'))[0].children[0].children[0].children[1].children[0].textContent;
+
+        if (latestTweet.indexOf("please retweet") != -1 || latestTweet.indexOf("Please retweet") != -1 || latestTweet.indexOf("Please Retweet") != -1 
+            || latestTweet.indexOf("please Retweet") != -1) {
+            latestTweet = latestTweet.substring(0, latestTweet.lastIndexOf("(please")); 
+        } else 
+            latestTweetNoDirectLink = latestTweet;
+
+        var latestTweetNoDirectLink;
+        if (latestTweet.indexOf("http") != -1) {
+            latestTweetNoDirectLink = latestTweet.substring(0, latestTweet.lastIndexOf("http")); 
+        } else 
+            latestTweetNoDirectLink = latestTweet;
+        
+        // latestTweetNoDirectLink = "\" " + latestTweetNoDirectLink + "\""
+
+        $('#twitter-feed-latest').text(latestTweetNoDirectLink);
+        $('#twitter-feed-latest').slideDown('slow');
+        $('#twitter-feed-link').removeClass('hide');
+    }
 });
