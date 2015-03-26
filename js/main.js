1920d02adc8ce8b4aab8aab3a915298b0da18bdd
Nirmal <njbenann@uwaterloo.ca>
2015-03-26 02:46:56 -0400

    Added link to blog

--- js/main.js
+++ js/main.js
@@ -4,15 +4,6 @@ $(document).ready(function () {
     var flag = 0;
     
     links.click(function (e) {
-        e.preventDefault();
-        var $this = $(this);
-        var section = $this.find('a').attr('href').substring(1);
-        $('.page-content').addClass('hide');
-        $('#'+section).removeClass('hide');
-
-        links.removeClass('active');
-        $this.addClass('active');
-
         var hamburgerButton = $('.navbar-header > button');
         if (hamburgerButton.is(':visible'))
         	hamburgerButton.trigger('click');	// Close the dropdown
